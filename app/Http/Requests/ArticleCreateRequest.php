69c2ccfa84c1c47712b23a93d2541be6d1e2c8d0
bashizade.official <bashizade.official@gmail.com>
2023-10-31 21:32:53 +0330

    fix article create and update request

--- app/Http/Requests/ArticleCreateRequest.php
+++ app/Http/Requests/ArticleCreateRequest.php
@@ -11,7 +11,7 @@ class ArticleCreateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class ArticleCreateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required',
+            'description' => 'required',
+            'category_id' => 'required',
+            'imageUrl' => 'required'
         ];
     }
 }
--- app/Http/Requests/ArticleUpdateRequest.php
+++ app/Http/Requests/ArticleUpdateRequest.php
@@ -11,7 +11,7 @@ class ArticleUpdateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class ArticleUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required',
+            'description' => 'required',
+            'category_id' => 'required',
+            'imageUrl' => 'required'
         ];
     }
 }
