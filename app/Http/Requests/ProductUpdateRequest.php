c5d08f36368f7c76f759420924715af6f38ce758
bashizade.official <bashizade.official@gmail.com>
2023-10-31 14:49:13 +0330

    fix product create and update request

--- app/Http/Requests/ProductUpdateRequest.php
+++ app/Http/Requests/ProductUpdateRequest.php
@@ -11,7 +11,7 @@ class ProductUpdateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class ProductUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'price_off' => 'required',
+            'category_id' => 'required',
         ];
     }
 }
