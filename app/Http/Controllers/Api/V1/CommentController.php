6fb46f8095f6cb6c92b033c89f7dd134aaebd346
bashizade.official <bashizade.official@gmail.com>
2023-10-31 22:15:55 +0330

    fix comment controller

--- app/Http/Controllers/Api/V1/CommentController.php
+++ app/Http/Controllers/Api/V1/CommentController.php
@@ -3,9 +3,36 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProductComment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CommentController extends Controller
 {
-    //
+    public function comments(): \Illuminate\Http\JsonResponse
+    {
+        $comments = ProductComment::all();
+        return $this->ResponseRequest('success','',[$comments]);
+    }
+
+    public function productComments($product_id): \Illuminate\Http\JsonResponse
+    {
+        $comments = ProductComment::query()->where('product_id',$product_id)->get();
+        return $this->ResponseRequest('success','',[$comments]);
+    }
+
+    public function change_status(Request $request,ProductComment $productComment): \Illuminate\Http\JsonResponse
+    {
+        $validator = Validator::make($request->all(),[
+           'status' => 'required'
+        ]);
+        if ($validator->fails())
+            return $this->ResponseRequest('error',$validator->errors()->all(),[],422);
+
+        $data = $request->all();
+
+        $productComment->update($data);
+
+        return $this->ResponseRequest('success','',['message' => 'status changed']);
+    }
 }
