61d6ba60a499ea2a3b4ae4c5b291f1ee67a01739
bashizade.official <bashizade.official@gmail.com>
2023-10-31 14:46:15 +0330

    create product create and update request . add create update delete products product methods to product controller

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -3,9 +3,42 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductCreateRequest;
+use App\Http\Requests\ProductUpdateRequest;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function products(): \Illuminate\Http\JsonResponse
+    {
+        $products = Product::query()->where('status',1)->get();
+        return $this->ResponseRequest('success','',[$products]);
+    }
+
+    public function product(Product $product): \Illuminate\Http\JsonResponse
+    {
+        return $this->ResponseRequest('success','',[$product]);
+    }
+    public function create(ProductCreateRequest $request): \Illuminate\Http\JsonResponse
+    {
+        $data_validated = $request->validated();
+        $product = Product::query()->create($data_validated);
+        return $this->ResponseRequest('success','',[$product]);
+    }
+
+    public function update(ProductUpdateRequest $request,Product $product): \Illuminate\Http\JsonResponse
+    {
+        $data_validated = $request->validated();
+        $pr = $product->query()->update($data_validated);
+        return $this->ResponseRequest('success','',[$pr]);
+    }
+
+    public function delete(Product $product): \Illuminate\Http\JsonResponse
+    {
+        $product->update([
+            'status' => 2
+        ]);
+        return $this->ResponseRequest('success','',['message' => 'product deleted']);
+    }
 }
